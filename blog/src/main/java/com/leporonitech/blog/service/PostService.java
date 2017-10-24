607dd665abe8323ef3a05524d20902d017807e4c
Leporoni <leporoni@hotmail.com>
2017-10-24 10:06:45 -0200

    Signed-off-by: Leporoni <leporoni@hotmail.com>

---
+++ blog/src/main/java/com/leporonitech/blog/service/PostService.java
@@ -0,0 +1,32 @@
+package com.leporonitech.blog.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leporonitech.blog.model.Post;
+import com.leporonitech.blog.repository.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public List<Post> findAll() {
+		return repository.findAll();
+	}
+
+	public Post findOne(Long id) {
+		return repository.findOne(id);
+	}
+
+	public Post save(Post post) {
+		return repository.saveAndFlush(post);
+	}
+
+	public void delete(Long id) {
+		repository.delete(id);
+	}
+}
